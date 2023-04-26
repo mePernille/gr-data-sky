24546619ed021b740c03a6796917b57ac46327ef
mePernille <89570696+mePernille@users.noreply.github.com>
2023-04-26 11:44:33 +0200

    tilføjet args og client

--- application.py
+++ application.py
@@ -12,7 +12,7 @@ import sys
 
 def client():
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Creating a UDP socket
-    serverAddr = (serverip, port)
+    serverAddr = ('127.0.0.1', 8083)
     try:
         clientSocket.connect(serverAddr)
 
@@ -37,10 +37,18 @@ def main():
 
     parser = argparse.ArgumentParser(description="Reliable Transport Protocol", epilog="End of thelp")
 
+    parser.add_argument('-s', '--server', action='store_true')
+    parser.add_argument('-c', '--client', action='store_true')
     
+
+    args = parser.parse_args()
+
     if args.server and args.client:
         print("you must either run the server or the client")
         sys.exit()
+
+    if args.server:
+        server(args.server)
 
 
 
