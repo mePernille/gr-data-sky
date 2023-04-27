f3f6a42b31ca9b875a5358da08e32b7461281125
mePernille <89570696+mePernille@users.noreply.github.com>
2023-04-27 10:19:17 +0200

    DRTP mini update

--- DRTP.py
+++ DRTP.py
@@ -5,14 +5,23 @@
 
 
 # 
-def stop_wait():
+def stop_wait(packet, header): # denne må tage ind headeren
+    while True:
+        packet.send() # sender en pakke
+        if data.resv() == "ACK": # hvis den modtager en ack
+            continue # fortsetter at sende pakker
+        else:
+            socket.settimeout(0.5)# venter i 500ms, må tage imot socket også
 
 
-    print("hei")
+    
 
 def GBN():
-    print("hei")
+    while True:
+        for i in range(0, len(packet), 5): # sender 5 biter av pakken om gangen
+            packet.sendto(serverIP, serverPort) #sender det til server
 
+        # tjekke pakke nr
 
 def SR():
     print("hei")    
